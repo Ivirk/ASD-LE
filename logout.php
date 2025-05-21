0c992a227ca8f2d9514b344db4e0b967b6622e44
Kervyyy <166726832+Ivirk@users.noreply.github.com>
2025-05-22 01:49:52 +0800

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,13 +1,6 @@
 <?php
-	function logout() {
-		if (isset($_SESSION['email'])) {
-			unset($_SESSION['email']);
-			unset($_SESSION['firstname']);
-			unset($_SESSION['lastname']);
-			unset($_SESSION['mobilenum']);
-			unset($_SESSION['password']);
-			session_destroy();
-			header("Location: index.php");
-		}
-	}
-?>
+session_start();
+session_unset();
+session_destroy();
+header("Location: admin_loginpage.php");
+exit();
